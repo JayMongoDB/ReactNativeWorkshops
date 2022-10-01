cc572de0fcffaf9f64fb8ee5dc7a20855a59ad81
Jeremy McGowan <jeremymcgowan@hotmail.com>
2022-10-01 10:43:10 -0700

    chore:initial commit

---
+++ screens/CampsiteInfoScreen.js
@@ -0,0 +1,11 @@
+import RenderCampsite from '../features/campsites/RenderCampsite';
+
+
+const CampsiteInfoScreen = ({ route }) => {
+
+    const { campsite } = route.params;
+
+    return <RenderCampsite campsite={campsite} />;
+};
+
+export default CampsiteInfoScreen;
