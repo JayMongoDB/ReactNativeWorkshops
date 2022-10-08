8bceb3c2c27c8649ad003130a3d75857ec6c66d5
Jeremy McGowan <jeremymcgowan@hotmail.com>
2022-10-08 12:46:38 -0700

    Workshop2

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,26 +1,20 @@
 import { ScrollView, Text } from "react-native";
-import { Card } from 'react-native-elements';
-
+import { Card } from "react-native-elements";
 
 const ContactScreen = () => {
-
-    return (
-        <ScrollView>
-            <Card wrapperStyle={{ margin: 20 }}>
-                <Card.Title>
-                    Contact Information
-                </Card.Title>
-                <Card.Divider />
-                <Text>1 Nucamp Way</Text>
-                <Text>Seattle,WA 98001</Text>
-                <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
-                <Text>Phone: 1-205-555-1234</Text>
-                <Text>Email: campsites@nucamp.co</Text>
-            </Card>
-        </ScrollView>
-    )
-
-
-}
+  return (
+    <ScrollView>
+      <Card wrapperStyle={{ margin: 20 }}>
+        <Card.Title>Contact Information</Card.Title>
+        <Card.Divider />
+        <Text>1 Nucamp Way</Text>
+        <Text>Seattle,WA 98001</Text>
+        <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
+        <Text>Phone: 1-205-555-1234</Text>
+        <Text>Email: campsites@nucamp.co</Text>
+      </Card>
+    </ScrollView>
+  );
+};
 
 export default ContactScreen;
